53e2b6cce761274d4238da2c21655644d30ff937
daiyunfei <1305037577@qq.com>
2021-11-24 15:36:02 +0800

    update

---
+++ src/day2/continue1.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class continue1
+{
+    public static void main(String[] args){
+        for(int a = 0;a<10;a++){
+            if(a%2==0){
+                System.out.println(a);
+                continue;
+            }
+           // System.out.println(a);
+        }
+    }
+}
---
+++ src/day2/shuzu.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class shuzu {
+    public static void main(String[] args){
+        int a[] = {1,2,3,4};
+        int b[];
+        //b=a;
+        b=a;
+        for(int i=0;i<b.length;i++){
+            System.out.println(b[i]);
+        }
+    }
+}
